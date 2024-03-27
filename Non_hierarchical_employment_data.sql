de63d74de7a3f934536e2e447948aa3a5872a3ef
emilianiilo <98584732+emilianiilo@users.noreply.github.com>
2024-03-27 14:49:21 +0200

    Delete Non_hierarchical_employment_data.sql

--- Non_hierarchical_employment_data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO non_hierarchical.employment (person_id, occupation_code, data)
-SELECT 
-    person_id,
-    occupation_code,
-    jsonb_build_object(
-        'start_time', start_time,
-        'end_time', end_time
-    ) AS data
-FROM traditional.employment;
