25d6f9dd8a5b01e6d7871d0385f98d9fcb70a51e
emilianiilo <98584732+emilianiilo@users.noreply.github.com>
2024-03-27 14:49:42 +0200

    Delete Non_hierarchical_employee_data.sql

--- Non_hierarchical_employee_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO non_hierarchical.employee (person_id, mentor_id, employee_status_type_code)
-SELECT 
-    person_id,
-    mentor_id,
-    employee_status_type_code
-FROM traditional.employee;
