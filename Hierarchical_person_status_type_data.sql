863d3fd27b81eeb26c0ad163c7d282e18a711ced
emilianiilo <98584732+emilianiilo@users.noreply.github.com>
2024-03-27 14:50:07 +0200

    Delete Hierarchical_person_status_type_data.sql

--- Hierarchical_person_status_type_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Person_status_type (person_status_type_code, data)
-SELECT person_status_type_code,
-       jsonb_build_object('name', name, 'description', description)
-FROM traditional.person_status_type;
