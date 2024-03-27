0d513ee45dd0c7f5778cb17001a56d84d85605e1
emilianiilo <98584732+emilianiilo@users.noreply.github.com>
2024-03-27 14:50:30 +0200

    Delete Hierarchical_employee_status_type_data.sql

--- Hierarchical_employee_status_type_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO hierarchical.employee_status_type (employee_status_type_code, data)
-SELECT 
-    employee_status_type_code,
-    jsonb_build_object(
-        'name', name,
-        'description', description
-    )
-FROM traditional.employee_status_type;
